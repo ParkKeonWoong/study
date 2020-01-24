9ea35bdbb5ddf88f0bb6b60067a1737319edce5d
parkkeon <parkkeonwoong@gmail.com>
2020-01-25 01:53:29 +0900

    UserDao_CountingConnection

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.service.CountingConnectionMaker;
 import com.example.demo.service.DaoFactory;
 import com.example.demo.service.UserDao;
 
@@ -16,22 +17,40 @@ public class DemoApplication {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		SpringApplication.run(DemoApplication.class, args);
+
+		DaoFactory factory = new DaoFactory();
+
+		
+		UserDao dao1 = factory.userDao();
+		UserDao dao2 = factory.userDao();
+		UserDao dao5 = factory.userDao();
+
+		System.out.println(dao1);
+		System.out.println(dao2);
 	
-	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-	UserDao dao = context.getBean("userDao",UserDao.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao",UserDao.class);
+		UserDao dao3 = context.getBean("userDao",UserDao.class);
+		UserDao dao4 = context.getBean("userDao",UserDao.class);
+		System.out.println(dao3);
+		System.out.println(dao4);
 
-
-	User user = new User();
-	user.setId("whiteship8");
-	user.setName("back8");
-	user.setPassword("married8");
-	dao.add(user);
-	System.out.println(user.getId() + " 등록 성공 ");
-	User user2 = dao.get(user.getId());
-	System.out.println(user2.getName());
-	System.out.println(user2.getPassword());
-	System.out.println(user2.getId() + " 조회 성공 ");
+		
+		
+		User user = new User();
+		user.setId("whiteship11");
+		user.setName("back9");
+		user.setPassword("married9");
+		dao.add(user);
+		System.out.println(user.getId() + " 등록 성공 ");
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " 조회 성공 ");
 		System.out.println( " 조회 성공 ");
+
+		CountingConnectionMaker ccm = context.getBean("connectionMaker",CountingConnectionMaker.class);
+		System.out.print("Counting = "+ccm.getCounter());
 
 
 		context.close();
---
+++ demo/src/main/java/com/example/demo/service/CountingConnectionMaker.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * CountingConnectionMaker
+ */
+public class CountingConnectionMaker implements ConnectionMaker {
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker){
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    public Connection makeConnection() throws ClassNotFoundException, SQLException{
+        this.counter++;
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter(){
+        return this.counter;
+    }
+    
+}
