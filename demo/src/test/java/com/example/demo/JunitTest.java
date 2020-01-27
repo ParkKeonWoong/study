e075a43820186219fff596f4e27e648bbc37835b
parkkeon <parkkeonwoong@gmail.com>
2020-01-27 18:09:27 +0900

    JunitTest_2

--- demo/src/test/java/com/example/demo/JunitTest.java
+++ demo/src/test/java/com/example/demo/JunitTest.java
@@ -5,34 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * JunitTest
  */
+@ContextConfiguration(locations = "/junitContext.xml")
 public class JunitTest {
     
+    @Autowired
+    ApplicationContext context;
+
     static Set<JunitTest> testObjects = new HashSet<JunitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test  
     public void test1(){
 		assertEquals(false, testObjects.contains(this));
         testObjects.add(this);
-        System.out.println(this);
+        assertEquals(contextObject == null || contextObject == this.context, true);
+        contextObject = this.context;
+
+        System.out.println(this.context);
+        System.out.println(contextObject);
     }
 
     @Test  
     public void test2(){
 		assertEquals(false, testObjects.contains(this));
         testObjects.add(this);
-        System.out.println(this);
+        assertEquals(contextObject == null || contextObject == this.context, true);
+        contextObject = this.context;
+
+        System.out.println(this.context);
+        System.out.println(contextObject);
     }
 
     @Test  
     public void test3(){
 		assertEquals(false, testObjects.contains(this));
         testObjects.add(this);
-        System.out.println(this);
+        assertEquals(contextObject , this.context);
+        contextObject = this.context;
+
+        System.out.println(this.context);
+        System.out.println(contextObject);
     }
 
     @Test  
